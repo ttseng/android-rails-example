24e34d8394ce1c1ba14bdba9ab3bc9fc9c6fe5a1
Tiffany Tseng <scientiffic@gmail.com>
2013-06-13 10:55:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phobos::Application.config.secret_token = 'c6224048d14c081454cad748f8d82d30709f082661858920c5cb47bf51718c0eea09c5c75f503e78b573e92dda821fb84428ae484156c8867a799a4f69b57b6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Phobos::Application.config.session_store :cookie_store, key: '_phobos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Phobos::Application.config.session_store :active_record_store
